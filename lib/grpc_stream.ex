8686c30e794cc2e5313345ca6b31a688bd7afd84
Adriano Santos <adriano.santos@v3.com.br>
2025-05-07 13:32:15 -0300

    chore: added more tests

--- lib/grpc_stream.ex
+++ lib/grpc_stream.ex
@@ -5,7 +5,7 @@ defmodule GrpcStream do
 
   ## Features
 
-    - Converts a gRPC request stream (`req_enum`) into a `Flow` with backpressure support.
+    - Converts a gRPC request stream (`input`) into a `Flow` with backpressure support.
     - Materializes the `Flow` into outgoing gRPC responses.
     - Supports optional unbounded producers to be attached for high-throughput streaming pipelines.
 
@@ -15,8 +15,8 @@ defmodule GrpcStream do
         use GRPC.Server, service: MyService.Service
         alias GrpcStream
 
-        def route_chat(req_enum, stream) do
-          GrpcStream.from(req_enum, max_demand: 10)
+        def route_chat(input, stream) do
+          GrpcStream.from(input, max_demand: 10)
           |> Flow.map(fn note ->
             # Process the incoming note
             # and prepare a response
@@ -35,11 +35,11 @@ defmodule GrpcStream do
         use GRPC.Server, service: MyService.Service
         alias GrpcStream
 
-        def stream_events(req_enum, stream) do
+        def stream_events(input, stream) do
           # Assuming you have a RabbitMQ or another unbounded source of data producer that is a GenStage producer
           {:ok, rabbitmq_producer} = MyApp.RabbitMQ.Producer.start_link([])
 
-          GrpcStream.from(req_enum, join_with: rabbit_producer, max_demand: 10)
+          GrpcStream.from(input, join_with: rabbit_producer, max_demand: 10)
           |> Flow.map(&transform_event/1)
           |> GrpcStream.materialize(stream)
         end
@@ -55,12 +55,16 @@ defmodule GrpcStream do
 
   @type t :: %__MODULE__{flow: Flow.t()}
 
+  @type item :: any()
+
+  @type reason :: any()
+
   @doc """
   Converts a gRPC request enumerable into a `Flow` pipeline with support for backpressure.
 
   ## Parameters
 
-    - `req_enum`: The enumerable stream of incoming gRPC messages (from `elixir-grpc`).
+    - `input`: The enumerable stream of incoming gRPC messages (from `elixir-grpc`).
     - `opts`: Optional keyword list for configuring the Flow or GenStage producer.
 
   ## Options
@@ -73,25 +77,29 @@ defmodule GrpcStream do
 
   ## Example
 
-      flow = GrpcStream.from(req_enum, max_demand: 5)
+      flow = GrpcStream.from(request, max_demand: 5)
 
   """
   @spec from(Enumerable.t(), Keyword.t()) :: Flow.t()
-  def from(req_enum, opts \\ []) do
+  def from(input, opts \\ [])
+
+  def from(input, opts) when is_list(input) do
     unbounded_producer = Keyword.get(opts, :join_with)
 
     case unbounded_producer do
       unbounded_pid when is_pid(unbounded_pid) ->
-        {:ok, pid} = GRPCStream.Producer.start_link(req_enum, opts)
+        {:ok, pid} = GRPCStream.Producer.start_link(input, opts)
 
         flow = Flow.from_stages([pid, unbounded_pid], opts)
         %__MODULE__{flow: flow}
 
       _ ->
-        flow = Flow.from_enumerable(req_enum, opts)
+        flow = Flow.from_enumerable(input, opts)
         %__MODULE__{flow: flow}
     end
   end
+
+  def from(input, opts), do: from([input], opts)
 
   @doc """
   Converts a `Flow` into a `GrpcStream`.
@@ -102,6 +110,7 @@ defmodule GrpcStream do
   @doc """
   Converts a `GrpcStream` into a `Flow`.
   """
+  @spec to_flow(t()) :: Flow.t()
   def to_flow(%__MODULE__{flow: flow}), do: flow
 
   @doc """
@@ -118,6 +127,7 @@ defmodule GrpcStream do
       GrpcStream.materialize(flow, stream)
 
   """
+  @spec materialize(t(), Stream.t(), Keyword.t()) :: :ok
   def materialize(%__MODULE__{flow: flow} = _stream, %Stream{} = from, opts \\ []) do
     is_dry_run? = Keyword.get(opts, :dry_run, false)
 
@@ -145,6 +155,7 @@ defmodule GrpcStream do
   is sent to the target GenServer, where `item` is the item from the GrpcStream.
   In other words, for GenServer's the contract is to send {:request, item} and wait for a response in the format {:response, msg}
   """
+  @spec ask(t(), pid | atom, non_neg_integer) :: t() | {:error, item(), reason()}
   def ask(stream, target, timeout \\ 5000)
 
   def ask(%__MODULE__{flow: flow}, target, timeout) when is_pid(target) do
@@ -156,10 +167,12 @@ defmodule GrpcStream do
           receive do
             {:response, res} -> res
           after
-            timeout -> {:error, :timeout}
+            timeout -> {:error, item, :timeout}
           end
 
         result
+      else
+        {:error, item, :not_alive}
       end
     end
 
@@ -167,13 +180,77 @@ defmodule GrpcStream do
   end
 
   def ask(%__MODULE__{flow: flow}, target, timeout) when is_atom(target) do
+    mapper = fn item ->
+      if function_exported?(target, :handle_call, 3) do
+        try do
+          case GenServer.call(target, {:request, item}, timeout) do
+            {:response, res} ->
+              res
+
+            other ->
+              {:error, item,
+               "Expected response from #{inspect(target)} to be in the format {:response, msg}. Found #{inspect(other)}"}
+          end
+        rescue
+          reason ->
+            {:error, item, reason}
+        end
+      else
+        {:error, item, "#{inspect(target)} must implement the GenServer behavior"}
+      end
+    end
+
+    %__MODULE__{flow: Flow.map(flow, mapper)}
+  end
+
+  @doc """
+  Same as ask/3 but with side-effects
+
+  Side Effect:
+
+  Exceptions thrown by this function will cause the Flow worker to crash, causing an EXIT that ends the pipeline and fails the test.
+
+  Caution: Prefer to use ask/3 and deal with the different types instead of throwing exceptions.
+  """
+  @spec ask!(t(), pid | atom, non_neg_integer) :: t()
+  def ask!(stream, target, timeout \\ 5000)
+
+  def ask!(%__MODULE__{flow: flow}, target, timeout) when is_pid(target) do
+    mapper = fn item ->
+      if Process.alive?(target) do
+        send(target, {:request, item, self()})
+
+        result =
+          receive do
+            {:response, res} -> res
+          after
+            timeout ->
+              raise "Timeout waiting for response from #{inspect(target)}"
+          end
+
+        result
+      else
+        raise "Target #{inspect(target)} is not alive. Cannot send request to it."
+      end
+    end
+
+    %__MODULE__{flow: Flow.map(flow, mapper)}
+  end
+
+  def ask!(%__MODULE__{flow: flow}, target, timeout) when is_atom(target) do
     if not function_exported?(target, :handle_call, 3) do
       raise ArgumentError, "#{inspect(target)} must implement the GenServer behavior"
     end
 
     mapper = fn item ->
-      {:response, res} = GenServer.call(target, {:request, item}, timeout)
-      res
+      case GenServer.call(target, {:request, item}, timeout) do
+        {:response, res} ->
+          res
+
+        _ ->
+          raise ArgumentError,
+                "Expected response from #{inspect(target)} to be in the format {:response, msg}"
+      end
     end
 
     %__MODULE__{flow: Flow.map(flow, mapper)}
@@ -187,26 +264,43 @@ defmodule GrpcStream do
     %__MODULE__{flow: Flow.filter(flow, filter)}
   end
 
-  @spec map(t(), (term -> term)) :: t
-  def map(%__MODULE__{flow: flow}, mapper) do
-    %__MODULE__{flow: Flow.map(flow, mapper)}
-  end
-
   @doc """
   Applies the given function mapping each input in parallel and
   flattening the result, but only one level deep.
   """
-  @spec flat_map(t, (term -> Enumerable.t())) :: t
+  @spec flat_map(t, (term -> Enumerable.t())) :: t()
   def flat_map(%__MODULE__{flow: flow}, flat_mapper) do
     %__MODULE__{flow: Flow.flat_map(flow, flat_mapper)}
   end
 
   @doc """
-  Applies the given function rejecting each input in parallel.
+  Applies the given function mapping each input.
   """
-  @spec reject(t, (term -> term)) :: t
-  def reject(%__MODULE__{flow: flow}, filter) do
-    %__MODULE__{flow: Flow.reject(flow, filter)}
+  @spec map(t(), (term -> term)) :: t()
+  def map(%__MODULE__{flow: flow}, mapper) do
+    %__MODULE__{flow: Flow.map(flow, mapper)}
+  end
+
+  @doc """
+  Creates a new partition for the given GrpcStream with the given options.
+
+  Every time this function is called, a new partition is created.
+  It is typically recommended to invoke it before a reducing function,
+  such as `reduce/3`, so data belonging to the same partition can be
+  kept together.
+
+  However, notice that unnecessary partitioning will increase memory
+  usage and reduce throughput with no benefit whatsoever. GrpcStream takes
+  care of using all cores regardless of the number of times you call
+  partition. You should only partition when the problem you are trying
+  to solve requires you to route the data around. Such as the problem
+  presented in `Flow`'s module documentation. If you can solve a problem
+  without using partition at all, that is typically preferred. Those
+  are typically called "embarrassingly parallel" problems.
+  """
+  @spec partition(t(), keyword()) :: t()
+  def partition(%__MODULE__{flow: flow}, options \\ []) do
+    %__MODULE__{flow: Flow.partition(flow, options)}
   end
 
   @doc """
@@ -218,5 +312,13 @@ defmodule GrpcStream do
   @spec reduce(t, (-> acc), (term, acc -> acc)) :: t when acc: term()
   def reduce(%__MODULE__{flow: flow}, acc_fun, reducer_fun) do
     %__MODULE__{flow: Flow.reduce(flow, acc_fun, reducer_fun)}
+  end
+
+  @doc """
+  Applies the given function rejecting each input in parallel.
+  """
+  @spec reject(t, (term -> term)) :: t
+  def reject(%__MODULE__{flow: flow}, filter) do
+    %__MODULE__{flow: Flow.reject(flow, filter)}
   end
 end
--- test/grpc_stream_test.exs
+++ test/grpc_stream_test.exs
@@ -2,15 +2,137 @@ defmodule GrpcStreamTest do
   use ExUnit.Case
   doctest GrpcStream
 
-  test "from/2 creates a flow from enumerable input" do
-    input = [%{message: "a"}, %{message: "b"}]
+  describe "simple test" do
+    test "from/2 creates a flow from a unary input" do
+      input = 1
 
-    flow =
-      GrpcStream.from(input, max_demand: 1)
-      |> GrpcStream.map(& &1)
-      |> IO.inspect(label: "Flow output")
+      result =
+        GrpcStream.from(input, max_demand: 1)
+        |> GrpcStream.map(& &1)
+        |> GrpcStream.materialize(%GRPC.Server.Stream{}, dry_run: true)
 
-    result = Enum.to_list(GrpcStream.to_flow(flow))
-    assert result == input
+      assert result == :ok
+    end
+
+    test "from/2 creates a flow from enumerable input" do
+      input = [%{message: "a"}, %{message: "b"}]
+
+      flow =
+        GrpcStream.from(input)
+        |> GrpcStream.map(& &1)
+
+      result = Enum.to_list(GrpcStream.to_flow(flow))
+      assert result == input
+    end
+  end
+
+  describe "from/2" do
+    test "converts a list into a flow" do
+      stream = GrpcStream.from([1, 2, 3])
+      assert %GrpcStream{} = stream
+
+      result = stream |> GrpcStream.map(&(&1 * 2)) |> GrpcStream.to_flow() |> Enum.to_list()
+      assert Enum.sort(result) == [2, 4, 6]
+    end
+  end
+
+  describe "ask/3 with pid" do
+    test "calls a pid and returns the response" do
+      pid =
+        spawn(fn ->
+          receive do
+            {:request, :hello, test_pid} ->
+              send(test_pid, {:response, :world})
+          end
+        end)
+
+      result =
+        GrpcStream.from([:hello])
+        |> GrpcStream.ask(pid)
+        |> GrpcStream.to_flow()
+        |> Enum.to_list()
+
+      assert result == [:world]
+    end
+
+    test "returns error if pid not alive" do
+      pid = spawn(fn -> :ok end)
+      # wait for the process to exit
+      ref = Process.monitor(pid)
+      assert_receive {:DOWN, ^ref, _, _, _}
+
+      result =
+        GrpcStream.from(["msg"])
+        |> GrpcStream.ask(pid)
+        |> GrpcStream.to_flow()
+        |> Enum.to_list()
+
+      assert result == [{:error, "msg", :not_alive}]
+    end
+  end
+
+  describe "ask/3 with GenServer" do
+    defmodule TestServer do
+      use GenServer
+
+      def start_link(_) do
+        GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+      end
+
+      def init(_), do: {:ok, %{}}
+
+      def handle_call({:request, value}, _from, state) do
+        {:reply, {:response, value}, state}
+      end
+    end
+
+    setup do
+      {:ok, _pid} = TestServer.start_link([])
+      :ok
+    end
+
+    test "asks GenServer and receives correct response" do
+      stream = GrpcStream.from(["abc"])
+
+      result =
+        stream
+        |> GrpcStream.ask(TestServer)
+        |> GrpcStream.to_flow()
+        |> Enum.to_list()
+
+      assert result == ["abc"]
+    end
+  end
+
+  describe "map/2, flat_map/2, filter/2" do
+    test "maps values correctly" do
+      result =
+        GrpcStream.from([1, 2, 3])
+        |> GrpcStream.map(&(&1 * 10))
+        |> GrpcStream.to_flow()
+        |> Enum.to_list()
+
+      assert Enum.sort(result) == [10, 20, 30]
+    end
+
+    test "flat_maps values correctly" do
+      result =
+        GrpcStream.from([1, 2])
+        |> GrpcStream.flat_map(&[&1, &1])
+        |> GrpcStream.to_flow()
+        |> Enum.to_list()
+
+      assert Enum.sort(result) == [1, 1, 2, 2]
+    end
+
+    test "filters values correctly" do
+      result =
+        GrpcStream.from([1, 2, 3, 4])
+        |> GrpcStream.filter(&(rem(&1, 2) == 0))
+        |> GrpcStream.to_flow()
+        |> Enum.to_list()
+
+      assert result == [2, 4]
+    end
   end
 end
